96c02a05712dd598a7a079f62f0f74dc7b61d3e2
Kadantsev Anton <kadantsev.anton@yandex.ru>
2023-10-08 00:51:58 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public float Speed = 0.01f; // Speed of camera (Lerp)
+    public Transform Camera; // Camera Object
+    public Transform Player; // Player Object
+
+    private void Update()
+    {
+        var cameraPosition = Camera.position; // Get position of Camera
+        cameraPosition.x = Mathf.Lerp(cameraPosition.x, Player.position.x, Speed); // Edit position
+        Camera.position = cameraPosition; // Update Camera position
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    public float Force; // Add force amount
+    public float jumpForce;
+    public float horizontalInput;
+    public float verticalInput;
+    public Rigidbody2D PlayerObject; // Player Object
+
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        AddForceToPlayer(new Vector2 (horizontalInput * Force, verticalInput * Force));
+        // move space sff
+        // move add
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AddForceToPlayer(new Vector2(0, jumpForce));
+        }
+    }
+    
+    private void AddForceToPlayer(Vector2 force)
+    {
+        if(force.y < 0) 
+        { 
+            force.y = 0;
+        }
+        PlayerObject.AddForce(force); // Add force to player
+    }
+}
